8951f5a71f7f47892e498f4ee7e5cd7b4337bd5f
Domesy <397163123@qq.com>
2019-07-13 19:12:59 +0800

    Hook的相关语法与使用，antd表单实现原理

---
+++ 03-componentization2/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import HookTest from './reactDay3/HookTest';
+import AntdFrom from './reactDay3/AntdFrom';
+import KeyFromTest from './reactDay3/KeyFromTest';
+
+function App() {
+  return (
+    <div>
+      {/* Hook相关语法 */}
+      {/* <HookTest /> */}
+
+      {/* antd表单 */}
+      {/* <AntdFrom /> */}
+
+      {/* 仿antd表单 */}
+      <KeyFromTest />
+    </div>
+ );
+}
+
+export default App;

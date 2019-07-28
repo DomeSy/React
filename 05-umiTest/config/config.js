fd426516c1705e5ec200466994f2739a55daf7a5
Domesy <397163123@qq.com>
2019-07-28 18:12:22 +0800

    umi+dva基本使用

--- 05-umiTest/config/config.js
+++ 05-umiTest/config/config.js
@@ -1,15 +1,31 @@
 // 可配置路由，在根目录下创建config文件夹下的config.js文件
 export default {
+  plugins: [
+    ['umi-plugin-react', {
+      // 按需加载
+      antd: true,
+      dva:true
+    }]
+  ],
   routes: [
     { path: "/", component: "./index" },
+    { path: "/login", component: "./login" },
+    { path: "/goods", component: "./goods" },
+    { 
+      path: "/about", component: "./about", 
+      // Routes与routes的区别在于./ 的不同，Routes的./是基于根目录，而routes是基于pages下的目录
+      Routes:[
+      './routes/PrivateRoute.js'  // 相对根目录
+    ] },
     {
       path: "users",
-      // 这里的./是相对于pages下的目录结构
       component: "./users/_layout",
       routes:[
         { path: "users/", component: "./users/index" },
         { path: "users/:id", component: "./users/$id" },
       ]      
-    }
+    },
+    // 配置404页面
+    { component: "./NotFound"}
   ]
 }
